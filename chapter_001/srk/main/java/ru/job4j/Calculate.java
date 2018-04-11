b38e8c1386537dca259fdfbd1ff4b6aafb57f0c4
Olgaasik <olgaas@bk.ru>
2018-04-11 16:39:21 +0700

    1.3. JavDoc. Code convetion.[#4087]

--- chapter_001/srk/main/java/ru/job4j/Calculate.java
+++ chapter_001/srk/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,17 @@
 package ru.job4j;
 
+    /** Calculate
+	 *
+	 * @author Olga Martynova (olgaas@bk.ru) 
+	 */
 public class Calculate {
-    
-    public static void main(String[] args) {
+	/** 
+	 *Main
+     * @param args - args.
+     */
+	 
+	public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
+    
